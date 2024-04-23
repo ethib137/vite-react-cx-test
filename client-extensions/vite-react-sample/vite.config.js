b98dea38512a4f4f652a2606390cea4a7f094a84
Evan Thibodeau <evan.thibodeau@liferay.com>
2024-04-23 17:16:55 +0400

    Update vite.config.js to work with the older React version and to output the JS files where the client extension expects the JS files to be.

--- client-extensions/vite-react-sample/vite.config.js
+++ client-extensions/vite-react-sample/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	build: {
+		outDir: 'build/vite',
+		rollupOptions: {
+			output: {
+				assetFileNames: 'assets/[name][extname]',
+				chunkFileNames: '[name]-[hash].js',
+				entryFileNames: '[name]-[hash].js',
+			},
+		},
+		target: 'esnext',
+	},
+	plugins: [react({jsxRuntime: 'classic'})],
 })
