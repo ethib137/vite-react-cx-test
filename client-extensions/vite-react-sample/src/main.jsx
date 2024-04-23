bf3e577fc53d25373a8c41589539c39f3e9716fa
Evan Thibodeau <evan.thibodeau@liferay.com>
2024-04-23 17:11:21 +0400

    Have main.jsx define and register a custom element.

--- client-extensions/vite-react-sample/src/main.jsx
+++ client-extensions/vite-react-sample/src/main.jsx
@@ -3,8 +3,20 @@ import * as ReactDOM from 'react-dom';
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const Main = () => (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+class WebComponent extends HTMLElement {
+	connectedCallback() {
+		ReactDOM.render(React.createElement(Main), this);
+	}
+}
+
+const ELEMENT_ID = 'vite-react-sample';
+
+if (!customElements.get(ELEMENT_ID)) {
+	customElements.define(ELEMENT_ID, WebComponent);
+}
